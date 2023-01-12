7e161f78aeba6e19cc8ea6240b35ed0581255065
Yevhen Komarov <e.komaroff@gmail.com>
2023-01-12 21:21:42 +0200

    deploy

---
+++ _next/static/bHt1Pic1eTmIjo0uxdwMQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e87f7bc44f47c51e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
